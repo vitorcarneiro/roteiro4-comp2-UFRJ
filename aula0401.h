13a926f0510be24dc78a40cc1070b45c83a9156b
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-01-04 15:53:44 -0300

    created prototype of Exponential calculator function

---
+++ aula0401.h
@@ -0,0 +1,24 @@
+/* 
+Universidade Federal do Rio de Janeiro
+Escola Politecnica
+Departamento de Eletronica e de Computacao
+EEL270 - Computacao II - Turma 2021/2
+Prof. Marcelo Luiz Drumond Lanza
+Autor: Vitor Carneiro Silva de Oliveira
+Descricao: Prototipo funcao CalcularExponencial
+
+$Author$
+$Date$
+$Log$
+
+*/
+
+#ifndef _AULA04_
+#define _AULA04_	"@(#)aula0401.h $Revision$"
+
+long double
+CalcularExponencial (double, int);
+
+#endif
+
+/* $RCSfile$ */
